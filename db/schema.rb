9772bdfb8ce345227631f736205e11b27873e818
bouendeu raphael <bouendeu_raphael@yahoo.fr>
2021-07-02 04:56:19 +0200

    add validation for post with spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_005516) do
+ActiveRecord::Schema.define(version: 2021_07_02_024155) do
+
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.date "date"
+    t.text "rationale"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
