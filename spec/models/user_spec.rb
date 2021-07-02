cc7d58e663287126c91713913ded3b732ba965de
bouendeu raphael <bouendeu_raphael@yahoo.fr>
2021-07-02 03:47:51 +0200

    user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+ describe "creation" do
+ before do
+   @user = User.create(email:"ab@yahoo.com",password:"cameroun",password_confirmation:"cameroun",first_name: "Bouendeu",last_name:"raphy")
+ end
+
+   it "can be created" do
+
+     expect(@user).to be_valid
+   end
+
+it "cannot be created without first_name,last_name" do
+    @user.first_name=nil
+    @user.last_name=nil
+    expect(@user).to_not be_valid
+end
+
+ end
+
 end
